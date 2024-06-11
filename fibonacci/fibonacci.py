6116f13237860a354f70743bdd64592a627bd332
AI-enthu <104884340+geminixmemory@users.noreply.github.com>
2024-06-11 13:01:01 +0800

    Add files via upload

---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,13 @@
+num_qty = int(input("Pls enter the quantity of Fibonacci numbers you want "))
+
+def comput(a):
+    fibo_list = []
+    orig_num = 1
+    new_num = 0
+    for x in range(a):
+        fibo_list.append(orig_num)
+        new_num = orig_num + new_num
+        orig_num = new_num
+    return fibo_list
+        
+print(comput(num_qty))
